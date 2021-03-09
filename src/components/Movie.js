12dd66d1b05abab25bbfcb4c7459e315b0ebe774
Mudassar Khan <mudassar_khan@hotmail.se>
2021-03-09 20:20:55 +0100

    extra

---
+++ src/components/Movie.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Movie extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Movie
+
+
