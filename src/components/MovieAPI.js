eef0509ab99ff053a39aeaa5cac4d0154d4960ab
Mkhaan97 <Mkhaan97@users.noreply.github.com>
2021-03-12 15:36:33 +0100

    Co-authored-by: Akhan91 <Akhan91@users.noreply.github.com>

--- src/components/MovieAPI.js
+++ src/components/MovieAPI.js
@@ -4,7 +4,8 @@ export class MovieAPI extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Posts</h1>
+                <button onClick={this.fetchData}>Fetch</button>
             </div>
         )
     }
